e391f358259400cab05813f8a45e5524e0e62158
KroMiose <li_xiangff@163.com>
2024-01-15 10:08:23 +0800

    split conf func

--- configs/config.py
+++ configs/config.py
@@ -1,33 +1,7 @@
 import os
 from typing import Literal
-from urllib.parse import quote_plus
 
-
-def gen_mysql_db_url(
-    host: str = "localhost",
-    port: int = 3306,
-    user: str = "root",
-    password: str = "",
-    database: str = "",
-    charset: str = "",
-) -> str:
-    """生成 MySQL 数据库连接 URL"""
-
-    user = quote_plus(user)
-    password = quote_plus(password)
-    database = quote_plus(database)
-    charset = quote_plus(charset)
-
-    return f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}{charset and f'?charset={charset}'}"
-
-
-def gen_sqlite_db_url(db_path: str) -> str:
-    """生成 SQLite 数据库连接 URL"""
-
-    if not db_path.startswith("/") and not db_path.startswith("./"):
-        db_path = f"./{db_path}"
-
-    return f"sqlite:///{db_path}"
+from configs.url_func import gen_mysql_db_url, gen_postgresql_db_url
 
 
 class Config:
